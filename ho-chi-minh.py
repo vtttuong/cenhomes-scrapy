e803f5280e6bdcfb53a4ac6ab822db2c31ddb527
hoahanie <69376027+hoahanie@users.noreply.github.com>
2020-11-05 22:28:40 +0700

    Add files via upload

---
+++ ho-chi-minh.py
@@ -0,0 +1,11 @@
+# import json
+# data={"hoa":"tuong"}
+# data1={"tuong":"hoa"}
+# with open ('bac-lieu.json','w') as f:
+#     f.write("[")
+#     json.dump(data,f,sort_keys=True, separators=(",",":"))
+#     json.dump(data1,f,sort_keys=True)
+#     f.write("]")
+#     f.close()
+# out_name= 'bac-lieu'
+# print(out_name+ '.json')
